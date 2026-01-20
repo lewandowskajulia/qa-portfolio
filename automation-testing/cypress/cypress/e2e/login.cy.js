26e2635428a7d8b943194741bb389fb179748ef6
lewandowskajulia <julialewandowska.sky@gmail.com>
2026-01-20 22:50:06 +0100

    improve login tests in Cypress

--- automation-testing/cypress/cypress/e2e/login.cy.js
+++ automation-testing/cypress/cypress/e2e/login.cy.js
@@ -1,41 +1,74 @@
 describe('Login', () => {
-    it('correct_login', () => {
-      cy.visit('https://www.saucedemo.com/')
-      cy.get('#user-name').type('standard_user')
-      cy.get('#password').type('secret_sauce')
+
+  const URL = 'https://www.saucedemo.com/'
+  const VALID_USER = 'standard_user'
+  const VALID_PASSWORD = 'secret_sauce'
+
+  beforeEach(() => {
+    cy.visit(URL)
+  })
+
+    it('should display login form', () => {
+      cy.get('#user-name').should('be.visible')
+      cy.get('#password').should('be.visible')
+      cy.get('#login-button').should('be.visible')
+    })
+
+    it('should log in successfully with valid credentials', () => {
+      cy.get('#user-name').type(VALID_USER)
+      cy.get('#password').type(VALID_PASSWORD)
       cy.get('#login-button').click()
       cy.url().should('include', 'https://www.saucedemo.com/inventory.html')
-    })
-    it('wrong_login', () => {
-      cy.visit('https://www.saucedemo.com/')
+    }) 
+
+    it('should mask password input', () => {
+       cy.get('#password').should('have.attr', 'type', 'password')
+    })
+
+    it('should submit login form when pressing Enter', () => {
+      cy.get('#user-name').type(VALID_USER)
+      cy.get('#password').type(`${VALID_PASSWORD}{enter}`)
+      cy.url().should('include', 'https://www.saucedemo.com/inventory.html')
+    })
+
+    it('should display an error for invalid username and password', () => {
       cy.get('#user-name').type('wrong')
       cy.get('#password').type('wrong')
       cy.get('#login-button').click()
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service')
+      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service')
     })
-    it('empty_fields', () => {
-      cy.visit('https://www.saucedemo.com/')
+
+    it('should display an error when username and password are empty', () => {
       cy.get('#login-button').click()
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Username is required')
+      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username is required')
     })
-    it('missing_password', () => {
-      cy.visit('https://www.saucedemo.com/')
-      cy.get('#user-name').type('standard_user')
+
+    it('should display an error when password is missing', () => {
+      cy.get('#user-name').type(VALID_USER)
       cy.get('#login-button').click()
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Password is required')
+      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Password is required')
     })
-    it('missing_username', () => {
-      cy.visit('https://www.saucedemo.com/')
-      cy.get('#password').type('wrong')
+
+    it('should display an error when username is missing', () => {
+      cy.get('#password').type(VALID_PASSWORD)
       cy.get('#login-button').click()
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Username is required')
+      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username is required')
     })
-    it('locked_out_user', () => {
-      cy.visit('https://www.saucedemo.com/')
+
+    it('should display an error for a locked out user', () => {
       cy.get('#user-name').type('locked_out_user')
-      cy.get('#password').type('secret_sauce')
+      cy.get('#password').type(VALID_PASSWORD)
       cy.get('#login-button').click()
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Sorry, this user has been locked out.')
+      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Sorry, this user has been locked out.')
     })
+
+    it('should log out the user and redirect to the login page', () => {
+      cy.get('#user-name').type(VALID_USER)
+      cy.get('#password').type(VALID_PASSWORD)
+      cy.get('#login-button').click()
+      cy.get('#react-burger-menu-btn').click()
+      cy.get('#logout_sidebar_link').click()
+      cy.url().should('eq', URL)
+    })
   })
   
