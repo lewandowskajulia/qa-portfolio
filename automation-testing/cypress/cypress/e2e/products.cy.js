a08570347e882042c4f5f0d696e7d51cfc4901b9
lewandowskajulia <julialewandowska.sky@gmail.com>
2026-01-22 19:43:24 +0100

    remove hardcoded product id

--- automation-testing/cypress/cypress/e2e/products.cy.js
+++ automation-testing/cypress/cypress/e2e/products.cy.js
@@ -1,19 +1,25 @@
 describe('Products', () => {
-    beforeEach(() => {
-      cy.visit('https://www.saucedemo.com/')
-      cy.get('#user-name').type('standard_user')
-      cy.get('#password').type('secret_sauce')
-      cy.get('#login-button').click()
-      cy.url().should('include', 'https://www.saucedemo.com/inventory.html')
-    })
-    it('powinno wyświetlić listę produktów', () => {
+
+  const URL = 'https://www.saucedemo.com/'
+  const VALID_USER = 'standard_user'
+  const VALID_PASSWORD = 'secret_sauce'
+
+  beforeEach(() => {
+    cy.visit(URL)
+    cy.get('#user-name').type(VALID_USER)
+    cy.get('#password').type(VALID_PASSWORD)
+    cy.get('#login-button').click()
+  })
+
+    it('should display a list of products', () => {
       cy.get('.inventory_item').should('have.length', 6) 
      })
   
-     it('powinno otworzyć stronę szczegółów produktu', () => {
+     it('should open product details page after clicking on product name', () => {
       cy.get('.inventory_item_name').first().click()
-      cy.url().should('include', '/inventory-item.html?id=4')
+      cy.url().should('include', '/inventory-item.html')
       cy.get('.inventory_details_name').should('be.visible')
       cy.get('.inventory_details_price').should('be.visible')
     })
+
 })
