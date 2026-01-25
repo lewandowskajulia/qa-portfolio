42fffec07be2c524eeb4b66b67f2373ab7744f7d
lewandowskajulia <julialewandowska.sky@gmail.com>
2026-01-25 23:24:35 +0100

    use fillCheckoutForm helper and constants to remove repetition

--- automation-testing/cypress/cypress/e2e/formpage.cy.js
+++ automation-testing/cypress/cypress/e2e/formpage.cy.js
@@ -1,139 +1,116 @@
-describe('Koszyk i checkout – SauceDemo', () => {
+describe('Cart and Checkout', () => {
+
+  const URL = 'https://www.saucedemo.com/'
+  const VALID_USER = 'standard_user'
+  const VALID_PASSWORD = 'secret_sauce'
+  const PRODUCTS_PAGE = 'https://www.saucedemo.com/inventory.html'
+  const CART = '.shopping_cart_link'
+
+    const FIRSTNAME = 'Jan'
+    const LASTNAME = 'Nowak'
+    const POSTALCODE = '12-345'
+
+  function fillCheckoutForm(first, last, postal) {
+    cy.get('[data-test="firstName"]').clear().type(first);
+    cy.get('[data-test="lastName"]').clear().type(last);
+    cy.get('[data-test="postalCode"]').clear().type(postal);
+    cy.get('[data-test="continue"]').click();
+  }
 
   beforeEach(() => {
-    cy.visit('https://www.saucedemo.com/');
-
-    cy.get('[data-test="username"]').type('standard_user');
-    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.visit(URL);
+    cy.get('[data-test="username"]').type(VALID_USER);
+    cy.get('[data-test="password"]').type(VALID_PASSWORD);
     cy.get('[data-test="login-button"]').click();
-
-    cy.url().should('include', '/inventory.html');
-
-    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.url().should('include', PRODUCTS_PAGE);
+    cy.get('[data-test^="add-to-cart"]').first().click();
     cy.get('.shopping_cart_badge').should('have.text', '1');
+    cy.get(CART).click();
+    cy.get('[data-test="checkout"]').click();
 
-    cy.get('[data-test="shopping-cart-link"]').click();
   });
 
-  describe('Wypełnienie formularza', () => {
+  describe('filling out the form', () => {
 
-    it('Poprawne wypełnienie formularza', () => {
-      cy.get('[data-test="checkout"]').click();
-      cy.get('[data-test="firstName"]').type('Qwe');
-      cy.get('[data-test="lastName"]').type('Qwe');
-      cy.get('[data-test="postalCode"]').type('123');
-      cy.get('[data-test="continue"]').click();
-
+    it('should fill out the form correctly', () => {
+      fillCheckoutForm(FIRSTNAME, LASTNAME, POSTALCODE);
       cy.contains('Checkout: Overview').should('be.visible');
     });
 
-    it('Wypełnienie formularza bez pierwszego imienia', () => {
-      cy.get('[data-test="checkout"]').click();
-      cy.get('[data-test="lastName"]').type('Qwe');
-      cy.get('[data-test="postalCode"]').type('123');
+    it('should show error when first name is missing', () => {
+      cy.get('[data-test="lastName"]').type(LASTNAME);
+      cy.get('[data-test="postalCode"]').type(POSTALCODE);
       cy.get('[data-test="continue"]').click();
-
-      cy.get('[data-test="error"]')
-        .should('have.text', 'Error: First Name is required');
+      cy.get('[data-test="error"]').should('have.text', 'Error: First Name is required');
     });
 
-    it('Wypełnienie formularza bez nazwiska', () => {
-      cy.get('[data-test="checkout"]').click();
-      cy.get('[data-test="firstName"]').type('Qwe');
-      cy.get('[data-test="postalCode"]').type('123');
+    it('should show error when last name is missing', () => {
+      cy.get('[data-test="firstName"]').type(FIRSTNAME);
+      cy.get('[data-test="postalCode"]').type(POSTALCODE);
       cy.get('[data-test="continue"]').click();
-
-      cy.get('[data-test="error"]')
-        .should('have.text', 'Error: Last Name is required');
+      cy.get('[data-test="error"]').should('have.text', 'Error: Last Name is required');
     });
 
-    it('Wypełnienie formularza bez kodu pocztowego', () => {
-      cy.get('[data-test="checkout"]').click();
-      cy.get('[data-test="firstName"]').type('Qwe');
-      cy.get('[data-test="lastName"]').type('Qwe');
+    it('should show error when postal code is missing', () => {
+      cy.get('[data-test="firstName"]').type(FIRSTNAME);
+      cy.get('[data-test="lastName"]').type(LASTNAME);
       cy.get('[data-test="continue"]').click();
-
-      cy.get('[data-test="error"]')
-        .should('have.text', 'Error: Postal Code is required');
+      cy.get('[data-test="error"]').should('have.text', 'Error: Postal Code is required');
     });
 
-    it('Zamknięcie komunikatu error oraz poprawne wypełnienie formularza', () => {
-      cy.get('[data-test="checkout"]').click();
-      cy.get('[data-test="firstName"]').type('Qwe');
-      cy.get('[data-test="lastName"]').type('Qwe');
+    it('should close error message and fill out the form correctly', () => {
+      cy.get('[data-test="firstName"]').type(FIRSTNAME);
+      cy.get('[data-test="lastName"]').type(LASTNAME);
       cy.get('[data-test="continue"]').click();
-
-      cy.get('[data-test="error"]')
-        .should('have.text', 'Error: Postal Code is required');
-
+      cy.get('[data-test="error"]').should('have.text', 'Error: Postal Code is required');
       cy.get('.error-button').click();
       cy.get('[data-test="error"]').should('not.exist');
-
-      cy.get('[data-test="postalCode"]').type('123');
+      cy.get('[data-test="postalCode"]').type(POSTALCODE);
       cy.get('[data-test="continue"]').click();
-
       cy.contains('Checkout: Overview').should('be.visible');
     });
 
   });
 
-  describe.only('Podsumowanie zamówienia i zakup produktu', () => {
+  describe('order summary and purchase', () => {
 
     beforeEach(() => {
-      cy.get('[data-test="checkout"]').click();
-      cy.get('[data-test="firstName"]').type('Qwe');
-      cy.get('[data-test="lastName"]').type('Qwe');
-      cy.get('[data-test="postalCode"]').type('123');
-      cy.get('[data-test="continue"]').click();
-
+      fillCheckoutForm(FIRSTNAME, LASTNAME, POSTALCODE);
       cy.get('.title').should('have.text', 'Checkout: Overview');
     });
 
-    it('Podsumowanie zamówienia zawiera produkt i cenę', () => {
+    it('should display product and price in order summary', () => {
       cy.get('.inventory_item_name').should('be.visible');
       cy.get('.inventory_item_price').should('be.visible');
       cy.get('.summary_total_label').should('be.visible');
     });
 
-    it('Cena produktu zgadza się z ceną produktu na podsumowaniu', () => {
-      cy.get('.inventory_item_price').invoke('text').then(itemPrice => {
-        cy.get('.summary_subtotal_label').invoke('text').then(summaryPrice => {
-          const itemPriceNumber = Number(itemPrice.replace('$', ''));
-          const summaryPriceNumber = Number(
-            summaryPrice.replace('Item total: $', '')
-          );
-
-          expect(itemPriceNumber).to.equal(summaryPriceNumber);
-        });
+    it('should match product price with order summary subtotal', () => {
+      cy.get('.inventory_item_price').invoke('text').then(itemPriceText => {
+      const itemPrice = parseFloat(itemPriceText.replace('$', ''));
+      cy.get('.summary_subtotal_label').invoke('text').then(summaryText => {
+      const summaryPrice = parseFloat(summaryText.replace('Item total: $', ''));
+      expect(itemPrice).to.equal(summaryPrice);
       });
     });
-
-    it('Do zakupu doliczany jest podatek', () => {
-      cy.get('.summary_subtotal_label').invoke('text').then(itemText => {
-        cy.get('.summary_tax_label').invoke('text').then(taxText => {
-          cy.get('.summary_total_label').invoke('text').then(totalText => {
-
-            const itemPrice = Number(itemText.replace('Item total: $', ''));
-            const tax = Number(taxText.replace('Tax: $', ''));
-            const total = Number(totalText.replace('Total: $', ''));
-
-            expect(total).to.equal(itemPrice + tax);
-          });
-        });
-      });
     });
 
-    it('Zakup produktu', () => {
+    it('should add tax to the order total', () => {
+      cy.get('.summary_subtotal_label').invoke('text').then(subtotalText => {
+      const subtotal = parseFloat(subtotalText.replace('Item total: $', ''));
+      cy.get('.summary_tax_label').invoke('text').then(taxText => {
+      const tax = parseFloat(taxText.replace('Tax: $', ''));
+      cy.get('.summary_total_label').invoke('text').then(totalText => {
+      const total = parseFloat(totalText.replace('Total: $', ''));
+      expect(total).to.equal(subtotal + tax);});
+      });
+    });
+    });
+
+    it('should complete the purchase successfully', () => {
       cy.get('[data-test="finish"]').click();
-
-      cy.contains('Thank you for your order!')
-        .should('be.visible');
-
-      cy.get('.complete-text')
-        .should(
-          'have.text',
-          'Your order has been dispatched, and will arrive just as fast as the pony can get there!'
-        );
+      cy.contains('Thank you for your order!').should('be.visible');
+      cy.get('.complete-text').should('have.text','Your order has been dispatched, and will arrive just as fast as the pony can get there!');
     });
 
   });
