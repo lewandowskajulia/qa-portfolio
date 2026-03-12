37d8f535122785f18b68c9a8dee809e3e0ab2552
lewandowskajulia <julialewandowska.sky@gmail.com>
2026-03-12 21:52:01 +0100

    remove test

--- automation-testing/cypress/cypress/e2e/home.cy.js
+++ automation-testing/cypress/cypress/e2e/home.cy.js
@@ -4,10 +4,11 @@ const homePage = new HomePage()
 const URL = 'https://www.saucedemo.com/'
 
 describe('Page', () => {
-  
+
   beforeEach(() => {
     homePage.visit()
   })
+
 
     it('should display correct page title for the login page and URL', () => {
       cy.title().should('eq', 'Swag Labs')
@@ -25,15 +26,16 @@ describe('Page', () => {
       homePage.getLogo().should('be.visible').and('contain.text', 'Swag Labs')
     })
 
-    it('should display login form', () => {
-      homePage.getUsernameInput().should('be.visible')
-      homePage.getPasswordInput().should('be.visible')
-      homePage.getLoginButton().should('be.visible')
-    })
 
     it('should display correct placeholders for login inputs', () => {
       homePage.getUsernameInput().should('have.attr', 'placeholder', 'Username')
       homePage.getPasswordInput().should('have.attr', 'placeholder', 'Password')
+    })
+
+    it('should display login form', () => {
+      homePage.getUsernameInput().should('be.visible')
+      homePage.getPasswordInput().should('be.visible')
+      homePage.getLoginButton().should('be.visible')
     })
     
   it('should display login page correctly on mobile viewport', () => {
