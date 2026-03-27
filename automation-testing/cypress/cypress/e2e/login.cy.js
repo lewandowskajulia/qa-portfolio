cd7d75f1110c4074e805be91472f054bb349cf96
lewandowskajulia <julialewandowska.sky@gmail.com>
2026-03-27 19:25:38 +0100

    new change

--- automation-testing/cypress/cypress/e2e/login.cy.js
+++ automation-testing/cypress/cypress/e2e/login.cy.js
@@ -44,14 +44,13 @@ describe('Login', () => {
       loginPage.getErrorMessage().should('be.visible').and('contain', 'Epic sadface: Username is required')
     })
 
-
     it('should display an error when username is missing', () => {
       loginPage.fillPassword(VALID_PASSWORD)
       loginPage.clickLogin()
       loginPage.getErrorMessage().should('be.visible').and('contain', 'Epic sadface: Username is required')
     })
 
-    
+
     it('should display an error when password is missing', () =>  {
       loginPage.fillUsername(VALID_USER)
       loginPage.clickLogin()
