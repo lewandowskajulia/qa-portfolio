746dfad647c092a9ef3b06abe409721dea328df9
lewandowskajulia <julialewandowska.sky@gmail.com>
2026-04-13 22:06:40 +0200

    new change

--- automation-testing/cypress/cypress/e2e/home.cy.js
+++ automation-testing/cypress/cypress/e2e/home.cy.js
@@ -36,11 +36,13 @@ describe('Page', () => {
       homePage.getLoginButton().should('be.visible')
     })
 
+
     it('should have correct input types', () => {
       homePage.getUsernameInput().should('have.attr', 'type', 'text')
       homePage.getPasswordInput().should('have.attr', 'type', 'password')
     })
 
+    
     it('should display login page correctly on mobile viewport', () => {
       cy.viewport('iphone-6')
       homePage.getLoginButton().should('be.visible')
